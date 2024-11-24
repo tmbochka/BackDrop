44dc9c43a3a9e4c9ff4a09648b49aa0f2018d202
tmbochka <julialatypova52@gmail.com>
2024-11-24 14:09:48 +0300

    BackDrop

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ const selectAll = (e) => document.querySelectorAll(e);
 
 const sections = selectAll('.rg__column');
 
-// create hover effect for each portfolio navigation item
+// для блока Удаление полностью, замена на цвет и оставшиеся
 const allLinks = gsap.utils.toArray('.portfolio__categories a');
 const pageBackground = select('.fill-background');
 const largeImage = select('.portfolio__image--l');
@@ -15,62 +15,37 @@ const smallImage = select('.portfolio__image--s');
 const lInside = select('.portfolio__image--l .image_inside');
 const sInside = select('.portfolio__image--s .image_inside');
 
+// для окошка "Добро пожаловать"
 const loader = select('.loader');
 const loaderInner = select('.loader .inner');
 const progressBar = select('.loader .progress');
 const loaderMask = select('.loader__mask');
 
-// images loaded
-function init() {
-    // show loader on page load
-    gsap.set(loader, { autoAlpha: 1 });
+// function init() {
+//     gsap.set(loader, { autoAlpha: 1 });
+//     gsap.set(loaderInner, { scaleY: 0.005, transformOrigin: 'bottom' }); // место с которого начинает выезжать синий квадрат при приветствии
+//     const progressTween = gsap.to(progressBar, { paused: true, scaleX: 0, ease: 'none', transformOrigin: 'right' });
+//     let loadedImageCount = 0, imageCount;
+//     const container = select('#main');
+//     const imgLoad = imagesLoaded(container);
+//     imageCount = imgLoad.images.length;
+//     updateProgress(0);
+//     imgLoad.on('progress', function () {
+//         loadedImageCount++;
+//         updateProgress(loadedImageCount);
+//     });
+//     function updateProgress(value) {
+//         gsap.to(progressTween, { progress: value / imageCount, duration: 0.3, ease: 'power1.out' })
+//     }
+//     imgLoad.on('done', function (instance) {
+//         gsap.set(progressBar, { autoAlpha: 0, onComplete: initPageTransitions });
+//     });
 
-    // scale loader down
-    gsap.set(loaderInner, { scaleY: 0.005, transformOrigin: 'bottom' });
+// }
 
-    // make a tween that scales the loader
-    const progressTween = gsap.to(progressBar, { paused: true, scaleX: 0, ease: 'none', transformOrigin: 'right' });
-
-    // setup variables
-    // https://codepen.io/desandro/pen/hlzaw
-    let loadedImageCount = 0, imageCount;
-    const container = select('#main');
-
-    // setup Images loaded
-    const imgLoad = imagesLoaded(container);
-    imageCount = imgLoad.images.length;
-
-    // set the initial progress to 0
-    updateProgress(0);
-
-    // triggered after each item is loaded
-    imgLoad.on('progress', function () {
-        // increase the number of loaded images
-        loadedImageCount++;
-        // update progress
-        updateProgress(loadedImageCount);
-    });
-
-    // update the progress of our progressBar tween
-    function updateProgress(value) {
-        // console.log(value/imageCount)
-        // tween progress bar tween to the right value
-        gsap.to(progressTween, { progress: value / imageCount, duration: 0.3, ease: 'power1.out' })
-    }
-
-    // do whatever you want when all images are loaded
-    imgLoad.on('done', function (instance) {
-        // we will simply init our loader animation onComplete
-        gsap.set(progressBar, { autoAlpha: 0, onComplete: initPageTransitions });
-    });
-
-}
-
-init();
+// init();
 
 function pageTransitionIn({ container }) {
-    // console.log('pageTransitionIn');
-    // timeline to stretch the loader over the whole screen
     const tl = gsap.timeline({
         defaults: {
             duration: 0.8,
@@ -86,8 +61,6 @@ function pageTransitionIn({ container }) {
 }
 
 function pageTransitionOut({ container }) {
-    // console.log('pageTransitionOut');
-    // timeline to move loader away down
     const tl = gsap.timeline({
         defaults: {
             duration: 0.8,
@@ -102,45 +75,38 @@ function pageTransitionOut({ container }) {
     return tl;
 }
 
+// переход между страничками
 function initPageTransitions() {
-    // do something before the transition starts
     barba.hooks.before(() => {
         select('html').classList.add('is-transitioning');
     });
-    // do something after the transition finishes
     barba.hooks.after(() => {
         select('html').classList.remove('is-transitioning');
     });
-
-    // scroll to the top of the page
     barba.hooks.enter(() => {
         window.scrollTo(0, 0);
     });
-
     barba.init({
         transitions: [{
             once() {
-                // do something once on the initial page load
                 initLoader();
             },
             async leave({ current }) {
-                // animate loading screen in
                 await pageTransitionIn(current);
             },
             enter({ next }) {
-                // animate loading screen away
                 pageTransitionOut(next);
             }
         }]
     });
 }
 
-function initLoader() {
 
+function initLoader() {
     const tlLoaderIn = gsap.timeline({
         id: 'tlLoaderIn',
         defaults: {
-            duration: 1.1,
+            duration: 1.1, // насколько долго появляется синий квадрат и надпись
             ease: 'power2.out'
         },
         onComplete: () => initContent()
@@ -168,7 +134,7 @@ function initLoader() {
     const tlLoaderOut = gsap.timeline({
         id: 'tlLoaderOut',
         defaults: {
-            duration: 1.2,
+            duration: 1.1, // насколько долго исчезает надпись
             ease: 'power2.inOut'
         },
         delay: 1
@@ -186,18 +152,10 @@ function initLoader() {
 
 }
 
-let currentAudio = null;
+// музыка
+let currentAudio = null; // проигрывается ли сейчас музыка
 
-function smoothScrollTo(url) {
-    gsap.to(window, {
-        duration: 1,
-        scrollTo: { y: 0 },
-        onComplete: () => {
-            window.location.href = url;
-        }
-    });
-}
-
+// обновляем текущее состояние: играет музыка или нет
 function updateMusicControlButton() {
     const musicControlButton = document.getElementById('musicControlButton');
     if (currentAudio && !currentAudio.paused) {
@@ -207,6 +165,7 @@ function updateMusicControlButton() {
     }
 }
 
+// событие при клике
 function initAudioControls() {
     const musicItems = document.querySelectorAll('.music-item');
     let currentAudio = null;
@@ -215,23 +174,30 @@ function initAudioControls() {
         item.addEventListener('click', () => {
             const audioId = item.dataset.audio;
             const audio = document.getElementById(audioId);
-            const backgroundImage = item.dataset.background;
 
             if (currentAudio && currentAudio !== audio) {
                 currentAudio.pause();
                 currentAudio.parentElement.classList.remove('playing');
+                console.log(`Paused audio: ${currentAudio.id}`);
             }
 
             if (audio.paused) {
                 audio.play();
                 currentAudio = audio;
                 item.classList.add('playing');
-                document.body.style.backgroundImage = `url(${backgroundImage})`;
+                console.log(`Playing audio: ${audio.id}`);
+
+                // Логирование завершения проигрывания
+                audio.addEventListener('ended', () => {
+                    console.log(`Audio ended: ${audio.id}`);
+                    currentAudio = null;
+                    item.classList.remove('playing');
+                });
             } else {
                 audio.pause();
                 currentAudio = null;
                 item.classList.remove('playing');
-                document.body.style.backgroundImage = '';
+                console.log(`Paused audio: ${audio.id}`);
             }
         });
     });
@@ -246,9 +212,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     initContent();
+    initAudioControls();
+
+    // Добавляем обработчик для вкладок
+    const tabButtons = document.querySelectorAll('.tab-button');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const targetTab = button.getAttribute('data-tab');
+
+            tabButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+
+            tabContents.forEach(content => {
+                if (content.id === targetTab) {
+                    content.style.display = 'block';
+                } else {
+                    content.style.display = 'none';
+                }
+            });
+        });
+    });
 });
-
-
 
 function initBackgroundChange() {
     const backgroundItems = document.querySelectorAll('.background-item');
@@ -279,37 +265,6 @@ function initBackgroundChange() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const backgroundItems = document.querySelectorAll('.background-item');
-    let currentBackground = null;
-
-    backgroundItems.forEach(item => {
-        const backgroundUrl = item.dataset.background;
-        const img = new Image();
-        img.src = backgroundUrl;
-
-        img.onload = () => {
-            item.style.backgroundImage = `url(${backgroundUrl})`;
-        };
-
-        item.addEventListener('mouseenter', () => {
-            console.log('Mouse enter:', backgroundUrl);
-            if (currentBackground !== backgroundUrl) {
-                document.body.style.setProperty('--background-image', `url(${backgroundUrl})`);
-                currentBackground = backgroundUrl;
-            }
-        });
-
-        item.addEventListener('mouseleave', () => {
-            console.log('Mouse leave:', backgroundUrl);
-            if (currentBackground) {
-                document.body.style.removeProperty('--background-image');
-                currentBackground = null;
-            }
-        });
-    });
-});
-
 function initContent() {
     select('body').classList.remove('is-loading');
     initSmoothScrollbar();
@@ -321,57 +276,40 @@ function initContent() {
     initPinSteps();
     initScrollTo();
     initAudioControls();
-    initBackgroundChange(); // Добавляем инициализацию изменения фона
+    initBackgroundChange();
 }
 
 const updateBodyColor = (color) => {
-    // gsap.to('.fill-background', { backgroundColor: color, ease: 'none'});
     document.documentElement.style.setProperty('--bcg-fill-color', color);
 }
 const getTextHeight = (textCopy) => {
     return textCopy.clientHeight;
 }
 
-// Smoooth Scrollbar
 function initSmoothScrollbar() {
-
     bodyScrollBar = Scrollbar.init(select('#viewport'), { damping: 0.07 });
-
-    // remove horizontal scrollbar
     bodyScrollBar.track.xAxis.element.remove();
-
-    // keep ScrollTrigger in sync with Smooth Scrollbar
     ScrollTrigger.scrollerProxy(document.body, {
         scrollTop(value) {
             if (arguments.length) {
-                bodyScrollBar.scrollTop = value; // setter
+                bodyScrollBar.scrollTop = value;
             }
-            return bodyScrollBar.scrollTop;    // getter
+            return bodyScrollBar.scrollTop;
         }
     });
-
-    // when the smooth scroller updates, tell ScrollTrigger to update() too: 
     bodyScrollBar.addListener(ScrollTrigger.update);
-
 }
 
 
-
-// Navigation Away - with updated trigger
 function initNavigation() {
-
     const mainNavLinks = gsap.utils.toArray('.main-nav a');
     const mainNavLinksRev = gsap.utils.toArray('.main-nav a').reverse();
 
     mainNavLinks.forEach(link => {
         link.addEventListener('mouseleave', e => {
-
-            // add class
             link.classList.add('animate-out');
-
         });
         link.ontransitionend = function () {
-            //remove class
             link.classList.remove('animate-out');
         }
     });
@@ -388,7 +326,6 @@ function initNavigation() {
         });
     }
 
-    // updated trigger to #main instead of absolute 100
     ScrollTrigger.create({
         trigger: '#main',
         start: 'top top-=100',
@@ -399,34 +336,24 @@ function initNavigation() {
         },
         onEnter: ({ direction }) => navAnimation(direction),
         onLeaveBack: ({ direction }) => navAnimation(direction),
-        // markers: true
     });
-
 }
 
-// Header Tilt
+// это для "попробуй все возможности фона"
 function initHeaderTilt() {
-
     select('header').addEventListener('mousemove', moveImages);
-
 }
 
+// а это для паралакса картинок
 function moveImages(e) {
-
     const { offsetX, offsetY, target } = e;
     const { clientWidth, clientHeight } = target;
-
-    // console.log(offsetX, offsetY, clientWidth, clientHeight);
-    // get 0 0 in the center
     const xPos = (offsetX / clientWidth) - 0.5;
     const yPos = (offsetY / clientHeight) - 0.5;
-
     const leftImages = gsap.utils.toArray('.hg__left .hg__image');
     const rightImages = gsap.utils.toArray('.hg__right .hg__image');
-
     const modifier = (index) => index * 1.2 + 0.5;
-
-    // move left 3 images
+    // левые
     leftImages.forEach((image, index) => {
         gsap.to(image, {
             duration: 1.2,
@@ -437,8 +364,7 @@ function moveImages(e) {
             ease: 'power3.out'
         });
     });
-
-    // move right 3 images
+    // правые
     rightImages.forEach((image, index) => {
         gsap.to(image, {
             duration: 1.2,
@@ -459,12 +385,9 @@ function moveImages(e) {
 
 }
 
-// Reveal Gallery
+// эффекты каждого блока главной страницы
 function initHoverReveal() {
-
     sections.forEach(section => {
-
-        // get componenets for animation
         section.imageBlock = section.querySelector('.rg__image');
         section.image = section.querySelector('.rg__image img');
         section.mask = section.querySelector('.rg__image--mask');
@@ -473,25 +396,20 @@ function initHoverReveal() {
         section.textMask = section.querySelector('.rg__text--mask');
         section.textP = section.querySelector('.rg__text--copy p');
 
-        // reset the initial position
         gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
         gsap.set([section.mask, section.textP], { yPercent: 100 });
         gsap.set(section.image, { scale: 1.2 });
 
-        // add event listeners to each section
         section.addEventListener('mouseenter', createHoverReveal);
         section.addEventListener('mouseleave', createHoverReveal);
 
     });
 }
 
+// для блока Удаление полностью, замена на цвет и оставшиеся
 function createHoverReveal(e) {
-    // console.log(e.type);
-
     const { imageBlock, mask, text, textCopy, textMask, textP, image, dataset } = e.target;
-
     const { color } = dataset;
-
     let tl = gsap.timeline({
         defaults: {
             duration: 1.8,
@@ -500,7 +418,6 @@ function createHoverReveal(e) {
     });
 
     if (e.type === 'mouseenter') {
-
         tl
             .to([mask, imageBlock, textMask, textP], {
                 yPercent: 0,
@@ -510,7 +427,6 @@ function createHoverReveal(e) {
             .to(image, { duration: 1.1, scale: 1 }, 0);
 
     } else if (e.type === 'mouseleave') {
-
         tl
             .to([mask, textP], { yPercent: 100 })
             .to([imageBlock, textMask], { yPercent: -101 }, 0)
@@ -518,12 +434,10 @@ function createHoverReveal(e) {
             .to(image, { scale: 1.2 }, 0);
 
     }
-
     return tl;
-
 }
 
-// Portfolio Hover
+// всё еще тот же блок
 function initPortfolioHover() {
     allLinks.forEach(link => {
         link.addEventListener('mouseenter', createPortfolioHover);
@@ -533,15 +447,7 @@ function initPortfolioHover() {
 }
 
 function createPortfolioHover(e) {
-
     if (e.type === 'mouseenter') {
-
-        // change images to the right urls
-        // fade in images
-        // all siblings to white and fade out
-        // active link to white
-        // update page background color
-
         const { color, imagelarge, imagesmall } = e.target.dataset;
         const allSiblings = allLinks.filter(item => item !== e.target);
         const tl = gsap.timeline({
@@ -555,34 +461,25 @@ function createPortfolioHover(e) {
             .to(e.target, { color: '#fff', autoAlpha: 1 }, 0);
 
     } else if (e.type === 'mouseleave') {
-
-        // fade out images
-        // all links back to black
-        // change background color back to default 
-
         const tl = gsap.timeline({
             onStart: () => updateBodyColor('#ACB7AE')
         });
         tl
             .to([largeImage, smallImage], { autoAlpha: 0 })
             .to(allLinks, { color: '#000000', autoAlpha: 1 }, 0);
-
     }
-
 }
 
 function createPortfolioMove(e) {
-
     const { clientY } = e;
-
-    // move large image
+    // большие фото
     gsap.to(largeImage, {
         duration: 1.2,
         y: getPortfolioOffset(clientY) / 6,
         ease: 'power3.out'
     });
 
-    // move small image
+    // маленькие фото
     gsap.to(smallImage, {
         duration: 1.5,
         y: getPortfolioOffset(clientY) / 3,
@@ -595,16 +492,10 @@ function getPortfolioOffset(clientY) {
     return -(select('.portfolio__categories').clientHeight - clientY);
 }
 
-// Parallax Images
+// блок технологии (это где про модель и все такое)
 function initImageParallax() {
-
-    // select all sections .with-parallax
     gsap.utils.toArray('.with-parallax').forEach(section => {
-
-        // get the image
         const image = section.querySelector('img');
-
-        // create tween for the image
         gsap.to(image, {
             yPercent: 20,
             ease: 'none',
@@ -614,14 +505,11 @@ function initImageParallax() {
                 scrub: true
             }
         });
-
     });
-
 }
 
-// Fixed navigation
+// чтобы навигаци в блоке Техонологии была закреплена
 function initPinSteps() {
-
     ScrollTrigger.create({
         trigger: '.fixed-nav',
         start: 'top center',
@@ -635,11 +523,8 @@ function initPinSteps() {
         const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
         return vh;
     }
-
     gsap.utils.toArray('.stage').forEach((stage, index) => {
-
         const navLinks = gsap.utils.toArray('.fixed-nav li');
-
         ScrollTrigger.create({
             trigger: stage,
             start: 'top center',
@@ -651,23 +536,21 @@ function initPinSteps() {
             onEnter: () => updateBodyColor(stage.dataset.color),
             onEnterBack: () => updateBodyColor(stage.dataset.color),
         });
-
     });
 
 }
 
+// а это чтобы делать прокрутку при нажатии на всё том же блоке Технологии + для последнего блока
 function initScrollTo() {
-    // find all links and animate to the right position
     gsap.utils.toArray('.fixed-nav a').forEach(link => {
         const target = link.getAttribute('href');
-
         link.addEventListener('click', (e) => {
             e.preventDefault();
             bodyScrollBar.scrollIntoView(select(target), { damping: 0.07, offsetTop: 100 });
         });
     });
 
-    // Добавляем обработчик для ссылки "Связаться с нами"
+    // вот для "Связаться с нами"
     const contactLink = document.querySelector('a[href="#0"]');
     if (contactLink) {
         contactLink.addEventListener('click', (e) => {
@@ -675,15 +558,4 @@ function initScrollTo() {
             bodyScrollBar.scrollIntoView(document.getElementById('contact-us'), { damping: 0.07, offsetTop: 100 });
         });
     }
-}
-
-function initBackgroundChange() {
-    const backgroundItems = document.querySelectorAll('.background-item');
-
-    backgroundItems.forEach(item => {
-        item.addEventListener('click', () => {
-            const backgroundColor = item.getAttribute('data-color');
-            document.body.style.backgroundColor = backgroundColor;
-        });
-    });
 }
