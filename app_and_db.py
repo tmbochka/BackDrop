a73c22c14f216f1e13711355d687303ad5852a57
tmbochka <julialatypova52@gmail.com>
2024-11-24 14:07:18 +0300

    files

--- app_and_db.py
+++ app_and_db.py
@@ -1,8 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.secret_key = 'hello'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BackDrop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
